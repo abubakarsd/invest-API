e378e3190b2af7e42c334f3cd62ffb31c7d86122
Abubakar Haruna Abdulmalik <42899173+abubakarsd@users.noreply.github.com>
2026-02-18 20:32:07 +0100

    feat: Add resend OTP route and import `resendOtp` controller.

--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,11 @@
-const express = require('express');
-const { register, login, getMe, logout, verifyEmail, updateDetails } = require('../controllers/auth');
+const { register, login, getMe, logout, verifyEmail, updateDetails, resendOtp } = require('../controllers/auth');
 const { protect } = require('../middleware/auth');
 
 const router = express.Router();
 
 router.post('/register', register);
 router.post('/verify-email', verifyEmail);
+router.post('/resend-otp', resendOtp);
 router.post('/login', login);
 router.get('/logout', logout);
 router.get('/me', protect, getMe);
