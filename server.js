777975220114f2597bf9d5e698eaacf6a5072a46
Abubakar Haruna Abdulmalik <42899173+abubakarsd@users.noreply.github.com>
2026-02-26 23:24:08 +0100

    chore: Update default server port and Vercel CORS origin.

--- server.js
+++ server.js
@@ -6,13 +6,13 @@ const cookieParser = require('cookie-parser');
 const morgan = require('morgan');
 
 const app = express();
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 app.use(cors({
-    origin: ['https://copytradehome.vercel.app', 'http://localhost:5173'], // Allow React frontend (Vite & CRA)
+    origin: ['https://invest-platform-omega.vercel.app', 'http://localhost:5173'], // Allow React frontend (Vite & CRA)
     credentials: true
 }));
 app.use(cookieParser());
